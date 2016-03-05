9faa2ae6e4af7c08a48d1f0e987df739d27c7d89
Levi Wilson <levi@leviwilson.com>
2016-03-04 23:36:07 -0500

    Guard against empty parameters

--- configure_elasticsearch.sh
+++ configure_elasticsearch.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -eu
+
+if [ -z "${ELASTICSEARCH_URL:-}" ]; then
+  echo "You must provide a value for ELASTICSEARCH_URL"
+  exit 1
+elif [ -z "${ELASTICSEARCH_NEO4J_INDEX:-}" ]; then
+  echo "You must provide a value for ELASTICSEARCH_NEO4J_INDEX"
+  exit 1
+fi
+
 echo "elasticsearch.host_name=${ELASTICSEARCH_URL-}" >> conf/neo4j.properties
 echo "elasticsearch.index_spec=${ELASTICSEARCH_NEO4J_INDEX-}" >> conf/neo4j.properties
 
