1e0da63b221c66bc8e71e1a62379ac790c1a30cc
Brian Underwood <ml+github@semi-sentient.com>
2016-09-20 13:22:15 -0400

    3.0 has a different conf file

--- configure_elasticsearch.sh
+++ configure_elasticsearch.sh
@@ -8,10 +8,10 @@ elif [ -z "${ELASTICSEARCH_NEO4J_INDEX:-}" ]; then
   exit 1
 fi
 
-echo "elasticsearch.host_name=${ELASTICSEARCH_URL-}" >> conf/neo4j.properties
-echo "elasticsearch.index_spec=${ELASTICSEARCH_NEO4J_INDEX-}" >> conf/neo4j.properties
-echo "elasticsearch.include_id_field=false" >> conf/neo4j.properties
-echo "elasticsearch.include_labels_field=false" >> conf/neo4j.properties
+echo "elasticsearch.host_name=${ELASTICSEARCH_URL-}" >> conf/neo4j.conf
+echo "elasticsearch.index_spec=${ELASTICSEARCH_NEO4J_INDEX-}" >> conf/neo4j.conf
+echo "elasticsearch.include_id_field=false" >> conf/neo4j.conf
+echo "elasticsearch.include_labels_field=false" >> conf/neo4j.conf
 
 if [[ "${ELASTICSEARCH_URL:-}" =~ ^https?://(.*)$ ]]; then
   /wait-for-it.sh -t "${WAIT_TIMEOUT:-30}" -s "${BASH_REMATCH[1]-}" -- echo "ElasticSearch is ready"
